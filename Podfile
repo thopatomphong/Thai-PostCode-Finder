c3c9f201cb3629dbce773bc738738deebd053b8a
Patomphong Wongkalasin <patomphong@cenergy.co.th>
2017-01-29 00:43:26 +0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '9.0'
+
+target 'PostCode' do
+use_frameworks!
+
+  pod 'SQLite.swift', '~> 0.11.2'
+
+  target 'PostCodeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PostCodeUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ PostCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCode
+//
+//  Created by Patomphong Wongkalasin on 1/28/2560 BE.
+//  Copyright © 2560 ReedUs Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
